e146d6cbc67162bbc5eef0d42c56ce8bfed641bb
shaban243 <shaban.mughal1056@gmail.com>
2024-10-08 19:52:56 +0500

    Modifynig commit message

--- All Projects/2-Node Advanced Concepts/Asynchronous Programming/AsyncProgm.js
+++ All Projects/2-Node Advanced Concepts/Asynchronous Programming/AsyncProgm.js
@@ -37,4 +37,65 @@
 // readFile();
 
 
+                                    // Streams //
 
+        //Readable stream
+// const fs = require('fs');
+
+// const stream = fs.createReadStream('largefile.txt', 'utf8');
+
+// stream.on("data", data  => {
+//     console.log("The data in chunks is read as following : \n", data);
+// });
+
+// stream.on("end", () => {
+//     console.log("The data is successfully read from the file!");
+// });
+
+// stream.on("error", (err) => {
+//     console.log("Error occured during reading the data from the file: ", err);
+// });
+
+
+// Writeable stream 
+
+// const fs = require('fs');
+
+// const writeStream = fs.createWriteStream('largfile.txt');
+
+// writeStream.write("I'm going to write some data in file. \n");
+// writeStream.write("I'm going in the mid of file to update content of file. \n");
+// writeStream.end();
+
+// writeStream.on('end', () => {
+//     console.log('Finishing writing content to the file!');
+// });
+
+// writeStream.on('error', (err) => {
+//     console.log('Error occurred in writing file!: ', err);
+// });
+
+
+
+                                // Duplex Stream
+// const {duplex} = require('stream');
+
+// const duplexStream = new Duplex({
+//     read(size) {
+//         this.push('This is a duplex stream!');
+//         this.push(null);
+//     }, 
+
+//     write(data, encoding, callback) {
+//         console.log("Data received from the file: ", data);
+//         callback();
+//     }
+
+// });
+
+// duplexStream.on('data', data => {
+//     console.log("Data read from the file: ", data);
+// });
+
+// duplexStream.write('Writing data to duplex stream!');
+// duplexStream.end();
