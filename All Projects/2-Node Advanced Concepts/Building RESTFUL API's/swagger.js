4b471e6d4dbd055d73349c08424680f36b45823f
shaban243 <shaban.mughal1056@gmail.com>
2024-10-09 19:17:35 +0500

    RESTFUL API's message

--- All Projects/2-Node Advanced Concepts/Building RESTFUL API's/swagger.js
+++ All Projects/2-Node Advanced Concepts/Building RESTFUL API's/swagger.js
@@ -1,36 +1,36 @@
-const swaggerJsdoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
+// const swaggerJsdoc = require('swagger-jsdoc');
+// const swaggerUi = require('swagger-ui-express');
 
 
-const swaggerDefinition = {
-    openapi: '3.0.0',
-    info: {
-        title: 'User API',
-        version: '1.0.0',
-        description: 'API documentation for Building RESTFUL APIs'
-    },
-    servers: [
-        {
-            url: 'http://localhost:3000',
-            description: 'Development server'
-        }
-    ]
-};
+// const swaggerDefinition = {
+//     openapi: '3.0.0',
+//     info: {
+//         title: 'User API',
+//         version: '1.0.0',
+//         description: 'API documentation for Building RESTFUL APIs'
+//     },
+//     servers: [
+//         {
+//             url: 'http://localhost:3000',
+//             description: 'Development server'
+//         }
+//     ]
+// };
 
 
-const options = {
-    swaggerDefinition,
-    apis: ['./server.js'],
-};
+// const options = {
+//     swaggerDefinition,
+//     apis: ['./server.js'],
+// };
 
 
-const swaggerSpec = swaggerJsdoc(options);
+// const swaggerSpec = swaggerJsdoc(options);
 
 
-module.exports = (app) => {
-    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-};
+// module.exports = (app) => {
+//     app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+// };
 
 
-const swaggerSetup = require('./swagger');
-swaggerSetup(app);
+// const swaggerSetup = require('./swagger');
+// swaggerSetup(app);
