5ca85dbd60e43392fde79991a8593fac091e72fa
shaban243 <shaban.mughal1056@gmail.com>
2024-10-14 21:21:14 +0500

    Modifying message

--- All Projects/2-Node Advanced Concepts/2.3-Authentication and Authorization/middleware/authmiddleware.js
+++ All Projects/2-Node Advanced Concepts/2.3-Authentication and Authorization/middleware/authmiddleware.js
@@ -1,16 +1,26 @@
-// const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 
-// const verifyToken = (req, res, next) => {
-//     const token = req.header('Authorization');
-//     if(!token) return res.status(400).json({message: 'Access denied, No token availabe!'});
-//     try {
-//         const decoded = jwt.verify(token, process.env.JWT_SECRET)
-//         req.user = decoded.id;
-//         next();
-//     } catch (err) {
-//         console.error(err.message);
-//         res.status(500).json({message: 'Server Error', Error: err.message});
-//     }
-// }   
+const verifyToken = (req, res, next) => {
+    const token = req.header('Authorization');
+    console.log(token);
+    if(!token) return res.status(400).json({message: 'Access denied, No token availabe!'});
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = { id: decoded.id, role: decoded.role };
+        next();
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).json({message: 'Server Error', Error: err.message});
+    }
+}   
 
-// module.exports = verifyToken;
+
+
+const isAdmin = (req, res, next) => {
+    const { role } = req.user;
+    if(role === 'admin') next();
+
+    return res.status(403).json({message: 'You are not allowed to permit the role'});
+}
+
+module.exports = verifyToken;
